705336948a63bfcf5117e1e0dadec7e4588ca30b
ducky <gillianthorne1353@gmail.com>
2026-07-01 12:54:45 -0400

    error logging for troubleshooting

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -38,6 +38,7 @@ router.post('/login', async (req, res) => {
 
         res.json({message: "Logged in successfully", user: req.session.user});
     } catch (err) {
+        console.log(err);
         res.status(500).json({ error: "Something went wrong" })
     }
 });
